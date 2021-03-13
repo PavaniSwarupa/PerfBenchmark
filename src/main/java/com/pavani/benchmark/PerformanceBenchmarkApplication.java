fbd30cd4dfe13971f9f1748be5795395ffe58918
anil.chillara <anil.chillara@outlook.com>
2021-03-13 11:24:45 -0700

    Initial Commit

---
+++ src/main/java/com/pavani/benchmark/PerformanceBenchmarkApplication.java
@@ -0,0 +1,13 @@
+package com.pavani.benchmark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerformanceBenchmarkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerformanceBenchmarkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pavani/benchmark/PerformanceBenchmarkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pavani.benchmark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerformanceBenchmarkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
